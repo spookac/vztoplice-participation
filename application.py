6556b86f9ea18c3f305bae254b915e53d4c7715a
spookac <tomislav.fotak@gmail.com>
2016-09-29 08:21:16 +0200

    py2exe setup

--- application.py
+++ application.py
@@ -1,5 +1,15 @@
+import os, sys
 from application.gui import Gui
 
+def resource_path(relative):
+	if hasattr(sys, "_MEIPASS"):
+		return os.path.join(sys._MEIPASS, relative)
+	return os.path.join(relative)
+	
+filename = "procedures.json"
+data_dir = "db"
+
+mydatafile = resource_path(os.path.join(data_dir, filename))
 
 gui = Gui("640", "480")
-gui.startApplication("db/procedures.json")
+gui.startApplication(mydatafile)
