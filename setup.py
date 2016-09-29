1fd60264baca6239b863b5f238a3f993a7471a08
spookac <tomislav.fotak@gmail.com>
2016-09-29 21:28:16 +0200

    py2exe setup file
    Works with Python 3.4.x

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-import py2exe, sys, os
-
-sys.argv.append('py2exe')
- 
-setup(
-    windows = ['application.py','application/gui/main.py','application/gui/add.py','application/gui/edit.py','application/gui/numberedit.py'],
-    zipfile = None,
-)
